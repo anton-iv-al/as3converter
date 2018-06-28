2e57d1724870421314ef1b291dda1d0c724e6ced
anton ivanov <anton_iv_al@mail.ru>
2018-06-28 17:05:40 +0300

    first

--- AS3Converter/MainWindow.xaml.cs
+++ AS3Converter/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,46 @@ namespace AS3Converter
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private string ConvertAS3(string src)
+        {
+            string res = src;
+            res = ConvertKeyWords(res);
+            res = ConvertVars(res);
+            res = ConvertFunctions(res);
+            return res;
+        }
+        
+        private string ConvertKeyWords(string src)
+        {
+            string res = src;
+            res = src.Replace(":Number", ":double");
+            return res;
+        }
+        
+        private string ConvertVars(string src)
+        {
+            string as3Pattern = @"var (\w+):(\w+)";
+            string unityPattern = @"$2 $1";
+            return Regex.Replace(src, as3Pattern, unityPattern);
+        }
+        
+        private string ConvertFunctions(string src)
+        {
+            string as3Pattern = @"function (\w+)(\(.*\)):(\w+)";
+            string unityPattern = @"$3 $1$2";
+            return Regex.Replace(src, as3Pattern, unityPattern);
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            string text = Clipboard.GetText();
+            if (text == null) return;
+
+            string converted = ConvertAS3(text);
+            Clipboard.SetDataObject(converted);
+            OutputLabel.Content = converted;
         }
     }
 }
