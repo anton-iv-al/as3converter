0362aa0ab8fed977a2626b553a665a697b62a6f5
anton <antonival96@gmail.com>
2018-08-26 06:28:41 +0300

    console

---
+++ AS3ToCSharp/Program.fs
@@ -0,0 +1,8 @@
+﻿open System.Windows
+open AS3ConverterF
+
+[<EntryPoint>]
+[<System.STAThread>]
+let main argv = 
+    Clipboard.SetData(DataFormats.Text, AS3ToCSharp.convert (Clipboard.GetText()))
+    0 // return an integer exit code
